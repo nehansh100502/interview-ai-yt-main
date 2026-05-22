3a3477fba8c3d79843cff3f6a13f9da610b66f16
nehansh100502 <nehansh100502@gmail.com>
2026-05-22 20:11:00 +0530

    done

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectToDB() {
+    console.log("MONGO URI:", process.env.MONGO_URI);
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ Connected to MongoDB");
